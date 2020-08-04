5f3268b0ad24062db06ffb15cea1b2d72642a972
elpen84 <elplist@gmail.com>
2020-08-03 18:42:46 -0700

    script update
    adding event li sterner

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // Set Date Inpute Min with Todays Date
 const today = new Date().toISOString().split("T")[0];
 
 dateEl.setAttribute("min", today);
+
+// takes values from form input
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle, countdownDate);
+}
+
+// event listener
+
+countdownForm.addEventListener("submit", updateCountdown);
