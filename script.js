ef1abed0a5f8fd3ce97b2066dd319c223da0b4c4
elpen84 <elplist@gmail.com>
2020-08-04 21:50:38 -0700

    Update script.js
    local storage function
    onload function

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 let countdownActive;
+let savedCountDown;
 
 const second = 1000;
 const minute = second * 60;
@@ -31,7 +32,7 @@ function updateDOM() {
   countdownActive = setInterval(() => {
     const now = new Date().getTime();
     const distance = countdownValue - now;
-    console.log("distance", distance);
+
     const days = Math.floor(distance / day);
     const hours = Math.floor((distance % day) / hour);
     const minutes = Math.floor((distance % hour) / minute);
@@ -66,14 +67,20 @@ function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
-  console.log(countdownTitle, countdownDate);
+  savedCountDown = {
+    title: countdownTitle,
+    date: countdownDate,
+  };
+
+  localStorage.setItem("countdown", JSON.stringify(savedCountDown));
   //check for valid date
+
   if (countdownDate === "") {
     alert("please select a date for the countdown.");
   } else {
     //Get number verison of current date, update DOM
     countdownValue = new Date(countdownDate).getTime();
-    console.log("countdown value:", countdownValue);
+
     updateDOM();
   }
 }
@@ -91,8 +98,23 @@ function reset() {
   countdownDate = "";
 }
 
+function restorePreviousCountdown() {
+  //get countdown from localstore if available
+  if (localStorage.getItem("countdown")) {
+    inputContainer.hidden = true;
+    savedCountDown = JSON.parse(localStorage.getItem("countdown"));
+    countdownTitle = savedCountdown.title;
+    countdownDate = savedCountDown.date;
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
+}
+
 // event listener
 
 countdownForm.addEventListener("submit", updateCountdown);
 countdownBtn.addEventListener("click", reset);
 completeBtn.addEventListener("click", reset);
+
+//onload check localstorage
+restorePreviousCountdown();
